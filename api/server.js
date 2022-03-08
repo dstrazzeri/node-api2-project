d018fd5224f4991456043a5756ea8136ae6e65a5
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-08 13:02:12 -0500

    push it real good

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+const server = express();
+
+server.use(express.json())
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'error not found'
+    })
+})
+
+module.exports = server;
