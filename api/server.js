94c785323bfcdcb65ba6c65b8e540760bfacf26f
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-08 13:18:33 -0500

    thats more like it

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const server = express();
 
 server.use(express.json())
 
+server.use('/api/posts', postsRouter)
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: 'error not found'
